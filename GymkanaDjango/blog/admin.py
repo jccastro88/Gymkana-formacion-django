1776182097e6af03c65b61e56d4aa7efb2be36bf
jccastro88 <jccastro88@gmail.com>
2019-06-17 11:42:56 +0200

    jccastro - Changed List Admin Panel for objects Nex and Event. Task 302886

--- GymkanaDjango/blog/admin.py
+++ GymkanaDjango/blog/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import New, Event
+from .models import Event, New
 
 # Register your models here.
 
-class
+
+class BaseAdmin(admin.ModelAdmin):
+    """
+    Basic class to inherit
+    """
+    search_fields = ['__all__']
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'start_date', 'end_date')
+
+
+class NewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'publish_date')
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(New, NewAdmin)
